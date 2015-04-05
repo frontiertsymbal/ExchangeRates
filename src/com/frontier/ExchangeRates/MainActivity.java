00f58bbe2164250ae8005982764871d17bcc2755
frontiertsymbal <frontier.dj@gmail.com>
2015-04-05 15:23:15 +0300

    add weekdays verify

--- src/com/frontier/ExchangeRates/MainActivity.java
+++ src/com/frontier/ExchangeRates/MainActivity.java
@@ -1,14 +1,18 @@
 package com.frontier.ExchangeRates;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import com.frontier.ExchangeRates.parseParams.*;
+import com.frontier.ExchangeRates.parseParams.Rates;
+import com.frontier.ExchangeRates.parseParams.RatesList;
+import com.frontier.ExchangeRates.parseParams.TodayParser;
+import com.frontier.ExchangeRates.parseParams.UsdToday;
 import com.frontier.ExchangeRates.util.Const;
 import com.frontier.ExchangeRates.util.DateBuilder;
-
-import com.frontier.ExchangeRates.webKit.UrlConnectionGetToJSonList;
+import com.frontier.ExchangeRates.webKit.GetRequestToJSonString;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -17,6 +21,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
+
+    /*TODO
+        dateBuilder only weekdays
+        today view
+        design
+        internet connection verify
+        * load more rates list *
+
+    */
 
     private List<String> dateList = DateBuilder.getDateList();
     private List<Rates> rates = new ArrayList<>();
@@ -38,14 +51,13 @@ public class MainActivity extends Activity {
             String archiveList;
             List<UsdToday> todayList;
 
-            todayList = TodayParser.parse(UrlConnectionGetToJSonList.getList(Const.TODAY_URL));
-
+            todayList = TodayParser.parse(GetRequestToJSonString.getString(Const.TODAY_URL));
 
             for (int i = 0; i < dateList.size(); i++) {
                 String aDateList = dateList.get(i);
                 date = aDateList;
 
-                archiveList = UrlConnectionGetToJSonList.getList(Const.ARCHIVE_URL_BODY + aDateList);
+                archiveList = GetRequestToJSonString.getString(Const.ARCHIVE_URL_BODY + aDateList);
                 rates.add(new Rates(date, gson.fromJson(archiveList, RatesList.class).getRatesList()));
             }
             return rates;
@@ -59,5 +71,12 @@ public class MainActivity extends Activity {
             intent.putExtra("rates", (Serializable) result);
             startActivity(intent);
         }
+    }
+
+    //TODO verify internet connection;
+
+    private boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() == null;
     }
 }
--- src/com/frontier/ExchangeRates/util/DateBuilder.java
+++ src/com/frontier/ExchangeRates/util/DateBuilder.java
@@ -19,19 +19,26 @@ public class DateBuilder {
 
         date.add(getDate(day, month, year));
 
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 31; i++) {
             int dayInt = calendar.get(Calendar.DATE);
-            calendar.set(Calendar.DATE, dayInt-1);
+
+            calendar.set(Calendar.DATE, dayInt - 1);
+
+            if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+                continue;
+            }
+
             dataAssign();
 
             date.add(getDate(day, month, year));
         }
+
         return date;
     }
 
     private static void dataAssign() {
         day = String.valueOf(calendar.get(Calendar.DATE));
-        month = String.valueOf(calendar.get(Calendar.MONTH)+1);
+        month = String.valueOf(calendar.get(Calendar.MONTH) + 1);
         year = String.valueOf(calendar.get(Calendar.YEAR));
     }
 
